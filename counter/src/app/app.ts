0e47814cca0d3c31bdd7932f65b9db73b60376fc
Jay-2810 <202201187@daiict.ac.in>
2026-01-02 09:07:10 +0530

    counter

---
+++ counter/src/app/app.ts
@@ -0,0 +1,44 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('counter');
+  count: number = 0;
+  incre() {
+    if (this.count >= 8)
+      this.count = 0;
+    else
+      this.count = this.count + 1;
+  }
+  decre() {
+    if (this.count <= -8)
+      this.count = 0;
+    else
+      this.count = this.count - 1;
+  }
+  reset() {
+    this.count = 0
+  }
+
+  handleCounter(val: string) {
+    if (val == 'minus') {
+      if (this.count <= 1)
+        this.count = 0;
+      else
+        this.count = this.count - 1;
+    } else if (val == 'plus') {
+      if (this.count >= 8)
+        this.count = 0;
+      else
+        this.count = this.count + 1;
+    } else {
+      this.count = 0;
+    }
+  }
+}
