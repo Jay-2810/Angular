70742636e6dcdbd68dc09a424efcba195c952e88
Jay-2810 <202201187@daiict.ac.in>
2026-01-09 12:25:53 +0530

    minor change

--- ifelse/src/app/app.ts
+++ ifelse/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Twowaybinding } from './twowaybinding/twowaybinding';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
-  imports: [Twowaybinding],
+  imports: [Twowaybinding, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
